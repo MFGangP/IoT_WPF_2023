313d13b3852ac72307deddf175e80e443efc8a66
PARK SEONG HYEON <lovehyun95@gmail.com>
2023-04-21 12:38:17 +0900

    2023.04.21 오전 수업 끝

---
+++ Day04/Day04WpfApp/WP08_personalInfoApp_mvvm/Logics/Commons.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WP08_personalInfoApp_mvvm.Logics
+{
+    internal class Commons
+    {
+        public static bool IsValidEmail(string email)
+        {
+            // xxxx@xxxxx.xxx 이메일 형식에 맞게 입력하도록 체크(검증체크 중 하나)
+            return true;
+        }
+
+        public static int GetAge(DateTime value)
+        {
+            // 입력된 날짜로 나이를 계산
+            return 30;
+        }
+
+        public static string GetZodiac(DateTime value)
+        {
+            // 입력된 생일로 12지신을 리턴
+            return "용띠";
+        }
+    }
+}
---
+++ Day04/Day04WpfApp/WP08_personalInfoApp_mvvm/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WP08_personalInfoApp_mvvm.Models
+{
+    internal class Person
+    {
+    }
+}
